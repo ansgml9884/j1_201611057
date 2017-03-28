7dcb945813099bf2ea40c79c5a6a8ca5895591ac
김문희 /j1_201611057 <ansgml9884@naver.com>
2017-03-28 14:19:25 +0900

    Delete MiniDuckSimulator1.java

--- MiniDuckSimulator1.java
+++
@@ -1,9 +0,0 @@
-package headfirst.designpatterns.strategy;
-
-public class MiniDuckSimulator1{
-  public static void main(String[] args) {
-    Duck mallard = new MallardDuck();
-    mallard.performFly();
-    mallard.performQuack();
-   }
-}
