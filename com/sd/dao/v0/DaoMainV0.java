67311fb429292f1f0da26fc7484051deac5b7737
김문희 /j1_201611057 <ansgml9884@naver.com>
2017-05-29 16:41:05 +0900

    Add files via upload

---
+++ com/sd/dao/v0/DaoMainV0.java
@@ -0,0 +1,35 @@
+package com.sd.dao.v0;
+
+import java.sql.*;
+import java.util.Properties;
+import java.io.*;
+
+public class DaoMainV0{
+  public static void main(String[] args){
+    Connection conn = null;
+    Statement stmt = null;
+    try {
+      String path = System.getProperty("user.dir");
+      path += "\\src\\mysql.properties";
+      Properties prop = new Properties();
+      prop.load(new FileInputStream(path));
+      Class.forName("com.mysql.jdbc.Driver");
+      conn = DriverManager.getConnection(
+         prop.getProperty("URL"),
+         prop.getProperty("USER"),
+         prop.getProperty("PASSWORD")
+       );
+      stmt = conn.createStatement();
+      String mySql = "CREATE TABLE persons(id int(64) NOT NULL AUTO_INCREMENT,";
+      mySql+="name varchar(255) NOT NULL, address varchar(255) NOT NULL, UNIQUE (id))";
+      stmt.execute(mySql);
+    } catch (Exception e){
+      e.printStackTrace();
+    } finally{
+      try{
+        if(stmt != null) stmt.close();
+        if(conn != null) conn.close();
+      } catch (Exception e){};
+    }
+  }
+}
